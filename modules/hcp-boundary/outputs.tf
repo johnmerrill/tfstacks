4bd0fdaf95cca5b54ff2de8d153eb6a6399cb87d
John Merrill <merrill454@gmail.com>
2024-12-15 23:38:05 -0500

    hcp boundary

--- modules/hcp-boundary/outputs.tf
+++ modules/hcp-boundary/outputs.tf
@@ -1,3 +1,11 @@
 output "boundary_cluster_url" {
   value = hcp_boundary_cluster.this.cluster_url
 }
+
+output "boundary_cluster_username" {
+  value = hcp_boundary_cluster.this.username
+}
+
+output "boundary_cluster_password" {
+  value = hcp_boundary_cluster.this.password
+}
